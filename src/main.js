e28fe40fb638b76a871f782aa960af7e1ddc74de
jingyu.yang <jingyu.yang@net263.com>
2020-05-12 09:49:01 +0800

    add: checkbox add disabled

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import MobileTree from "../lib/vue-mobile-tree.umd"
-import "../lib/vue-mobile-tree.css";
+// import MobileTree from "../lib/vue-mobile-tree.umd"
+// import "../lib/vue-mobile-tree.css";
+
+import MobileTree from "../src/components";
 Vue.use(MobileTree);
 
 Vue.config.productionTip = false
